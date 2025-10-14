0db3c058a1eb9d2cd8c54371ab7bccb0f2f7090a
Marcel Andersen <marc449b@hotmail.com>
2025-10-14 14:01:35 +0200

    Initial Commit - 2 (needed this project transferred to other PC)

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      // path: all else than stated above
+      path: '/:pathMatch(.*)',
+      name: '404',
+      component: () => import('../views/NotFoundView.vue'),
+    },
   ],
 })
 
